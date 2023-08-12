a873fb2f708bd14946f40e5eee5851d17d4000d3
rakytion <criss98la2@gmail.com>
2023-08-12 16:10:31 -0300

    rasguñando la aprobada

--- tps/tp objetos.py
+++ tps/tp objetos.py
@@ -59,4 +59,37 @@ mate.beber(mate1)
 mate.cebar(mate1)
 mate.beber(mate1)
 
+#sacacorchos
 
+class Corcho:
+    def __init__(self, Bodega):
+        self.Bodega=Bodega
+    
+    def __str__(self):
+        return self.Bodega
+
+class Botella:
+    def __init__(self, Corcho:Corcho):
+        self.CorchoDeBotella=Corcho
+
+class Sacacorcho:
+    def __init__(self):
+        self.CorchoSacacorcho=None
+    
+    def Destapar(self,Botella:Botella):
+        if self.CorchoSacacorcho==None:
+            if Botella.CorchoDeBotella!=None:
+                self.CorchoSacacorcho=Botella.CorchoDeBotella
+                Botella.CorchoDeBotella=None
+                print("Botella destapada")
+            else:
+                print("La botella ya esta destapada")
+        else:
+            print("El sacacorchos ya tiene un corcho, primero hay que limpiarlo")
+            
+    def Limpiar(self):
+        if self.CorchoSacacorcho==None:
+            print("El sacacorchos ya esta limpio")
+        else:
+            self.CorchoSacacorcho=None
+            print("Sacacorcho limpiado")
